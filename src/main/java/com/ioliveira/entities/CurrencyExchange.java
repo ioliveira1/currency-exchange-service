903af42f022bb0989f5f1e130366f17cbfb8c338
Igor Oliveira <igorfoliveira@gmail.com>
2020-07-10 19:13:50 -0300

    configurando banco de dados e inicializando os dados

--- src/main/java/com/ioliveira/entities/CurrencyExchange.java
+++ src/main/java/com/ioliveira/entities/CurrencyExchange.java
@@ -2,15 +2,26 @@ package com.ioliveira.entities;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder(toBuilder = true)
+@Entity
 public class CurrencyExchange {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "Currency_from")
     private String from;
+
+    @Column(name = "Currency_to")
     private String to;
     private Integer conversionMultiple;
+
+    @Transient //Nao sera gravado no banco
     private int port;
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CURRENCY_EXCHANGE VALUES
+(null, 65, 'USD', 'INR'),
+(null, 75, 'EUR', 'INR'),
+(null, 25, 'AUD', 'INR');
