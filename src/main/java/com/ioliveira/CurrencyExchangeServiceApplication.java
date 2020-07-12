e2d3623a6a3c5a0df0bc92c04d7a1108db727c42
Igor Oliveira <igorfoliveira@gmail.com>
2020-07-12 17:56:43 -0300

    implementando Spring Cloud Sleuth

--- src/main/java/com/ioliveira/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/ioliveira/CurrencyExchangeServiceApplication.java
@@ -1,8 +1,10 @@
 package com.ioliveira;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +12,12 @@ public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+	//Trace de todas as requests
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
